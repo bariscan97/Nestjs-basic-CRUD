af328c9638cc510e56e5749012df5c5bc048e9e3
bariscan97 <bariscan9788@gmail.com>
2024-01-10 14:06:53 +0300

    update

--- src/repo/repo.module.ts
+++ src/repo/repo.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RepoService } from './repo.service';
 import { MysqlModule } from 'nest-mysql';
-
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [MysqlModule.forRoot({
-    host: 'localhost',
-    user: 'root',
-    database: 'mybase',
-    password: "Dekar123."
+    host: ConfigService.get<string>("db_host"),
+    user: ConfigService.get<string>("db_user"),
+    database: ConfigService.get<string>("database"),
+    password: ConfigService.get<string>("db_password")
     
   })],
   
